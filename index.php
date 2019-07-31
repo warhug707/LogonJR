e61c9f07e5a98fefac5680528af32074e4386d21
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-07-31 09:38:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <html>
-<head><title>reeeee</title></head>
+<head><title></title></head>
 <body>
-  <h1>FIIIIIIIYAAAAAAAAAAAAAAAh!!</h1>
-  <p>this is very interesting</p>
+ <main>
+   <h1>Hello Guys What is up!!</h1>
+   <header>
+    <h1>Welcome to my Website</h1>
+    <p>Where you can see anything</p>
+   </header>
+     <footer>
+       <h3>Thank you</h3>
+       <p>You have visited my site and I am greatfull</p>
+     </footer>
 </body>
 </html>
