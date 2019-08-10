25e89d85e6287092c2a7f81db54096dea61d6f46
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-10 17:56:00 +0800

    Update photogallery.php

--- photogallery.php
+++ photogallery.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
-<html>
-<head><title</title>
-</head>
+<html lang="en">
+<head><title>OLA!!</title></head>
 <body>
- <a href="index.php">BRUH</a>
+ <main>
+   <h1>Hello Guys What is up!!</h1>
+   <header>
+    <h1>Welcome to my Website</h1>
+    <p>Where you can see anything but not anyone</p>
+   </header>
+     <footer>
+       <h3>Thank you</h3>
+       <p>You have visited my site and I am greatfull</p><br>
+       <p>Made with love and Care</p>
+     </footer>
+ </main>
 </body>
 </html>
