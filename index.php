25e09e6219ea2ee103370ea10f801fcf2b85d93b
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-10 18:22:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><title>OLA!!</title></head>
+<head><title>BRUH!!</title></head>
 <body>
- <main>
-   <h1>Hello Guys What is up!!</h1>
-   <header>
-    <h1>Welcome to my Website</h1>
-    <p>Where you can see anything but not anyone</p>
-   </header>
-     <footer>
-       <h3>Thank you</h3>
-       <p>You have visited my site and I am greatfull</p><br>
-       <p>Made with love and Care</p>
-     </footer>
- </main>
+ <a href="">BRUH</a>
 </body>
 </html>
