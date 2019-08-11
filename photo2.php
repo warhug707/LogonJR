ffa657370f741d3b88db7abe904f5fe0ebedd2e5
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-12 00:20:50 +0800

    Update photo2.php

--- photo2.php
+++ photo2.php
@@ -4,7 +4,7 @@
 </head>
 <body>
  <center><h1>LogonJR Photo Gallery</h1>
-   <img src=""></center>
+   <img src="anime_render_118_by_michelleurs-d96znd8.png"></center>
  <a href="index.php">HOME</a>
  <a href="photo2.php" style="float: right;">NEXT PHOTO</a>
 </body>
