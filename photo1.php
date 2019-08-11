3c638e573e1920f353630fd35351feb038807a22
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-12 00:17:47 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -6,6 +6,6 @@
  <center><h2>LogonJR Photo Gallery</h2>
    <img src="rygg92n25ye31.jpg"></center>
  <a href="index.php">HOME</a>
- <a href="photo2.php" align="right">NEXT PHOTO</a>
+ <a href="photo2.php" style="float: right;">NEXT PHOTO</a>
 </body>
 </html>
