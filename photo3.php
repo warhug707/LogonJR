31d68c865dc68e48a2ff4aee469cf353fb6d8292
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-12 00:21:12 +0800

    Update photo3.php

--- photo3.php
+++ photo3.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html>
-<head><title></title>
+<html lang="en">
+<head><title>P2</title>
 </head>
 <body>
- <center><h2>LogonJR Photo Gallery</h2>
-   <img src=""></center>
- <a href="">
- <a href="">
+ <center><h1>LogonJR Photo Gallery</h1>
+   <img src="anime_render_118_by_michelleurs-d96znd8.png"></center>
+ <a href="index.php">HOME</a>
+ <a href="photo2.php" style="float: right;">NEXT PHOTO</a>
 </body>
 </html>
