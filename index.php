2c510373b97d92bae0a9385ee8ae6b1ff23b4041
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-12 00:01:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><title>BRUH!!</title></head>
+<head><title></title></head>
 <body>
- <a href="https://www.youtube.com">BRUH</a>
+ <center><h1>LogonJR Photo Gallery</h1>
+  <h3>List of Photos</h3>
+     <a href="">Photo #1</a>
+     <a href="">Photo #2</a>
+     <a href="">Photo #3</a>
 </body>
 </html>
