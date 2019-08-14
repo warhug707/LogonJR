b6f8292b42981371588669fa07c52b05aeebe9cd
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-14 21:09:19 +0800

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -7,9 +7,5 @@
      <a href="photo1.php">Photo #1</a><br/>
      <a href="photo2.php">Photo #2</a><br/>
      <a href="photo3.php">Photo #3</a><br/>
- <form action="index.php>
-               <label for="log">This is not you?</label>
-  <button type="submit" name="log">Log Out</button>
- </form>
 </body>
 </html>
