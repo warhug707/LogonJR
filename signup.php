c1be7962574de67c65eca2886081d8e1cfcbab11
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-14 20:51:39 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -10,13 +10,13 @@
  <form>
   <p>Please fill in this form to create an account.</p>
   <label for="email"><b>Email Address</b></label>
-  <input type="text" placeholder="Enter Email" name="email" required>
+  <input type="text" placeholder="Enter Email" name="email" required><br/>
   <label for="cemail"><b>Confirm Email Address</b></label>
-  <input type="text" placeholder="Re-Enter Email" name="cemail" required>
+  <input type="text" placeholder="Re-Enter Email" name="cemail" required><br/>
   <label for="pass"><b>Password</b></label>
-  <input type="password" placeholder="Enter Password" name="pass" required>
+  <input type="password" placeholder="Enter Password" name="pass" required><br/>
   <label for="cpass"><b>Confirm Password</b></label>
-  <input type="password" placeholder="Re-Enter Password" name="cpass" required>
+  <input type="password" placeholder="Re-Enter Password" name="cpass" required><br/>
   <button type="submit">Submit</button>
  </form>
 </main>
