ba5568f50fac93076fb5d01183486bb65f15eda3
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-14 09:38:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,20 @@
 </head>
 <body>
 <main align="center">
- <form>
-   <label for="user"><b>Username:</b></label>
-   <input type="text" placeholder="Enter Username" name="user" required><br/>
+ <h1>Login</h1>
+<form >
    
-   <label for="pass"><b>Password:</b></label>
-   <input type="password" placeholder="Enter Password" name="pass" required><br/>
-   <button type="submit">Login</button><br/><br/>
+<label for="user"><b>Username:</b></label>
+<input type="text" placeholder="Enter Username" name="user" required><br/>
    
-   <a href="">Reset Password</a><br/><br/>
-   <a href="">Sign up</a>
+<label for="pass"><b>Password:</b></label>
+<input type="password" placeholder="Enter Password" name="pass" required><br/>
+   
+<button type="submit">Login</button><br/><br/>
+<br/>   
+   
+<a href="">Reset Password</a><br/><br/>
+<a href="">Sign up</a>
+</form>
 </body>
 </html>
