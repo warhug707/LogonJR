bfeedee5a12e97e0506b2e3529490f981010c0be
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-14 09:27:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head><title>LogonJR</title>
+</head>
+<body>
+<main>
+ <form>
+   <label for="user"><b>Username:</b></label>
+   <input type="text" placeholder="Enter Username name="user" required>
+   
+   <label for="pass"><b>Passwor:</b></label>
+   <input type="password" placeholder="Enter Password" name="pass" required>
+   <button type="submit">Login</button>
+   
+   <a href="" align="cener">Reset Password</a>
+   <a href="" align="center">Sign up</a>
+</body>
+</html>
