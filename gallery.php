b9a324915f7f307aa005690b77c92eeb7ffd70eb
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-14 21:07:30 +0800

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head><title>HOME PAGE</title></head>
-<body>
- <center><h1>LogonJR Photo Gallery</h1>
+<body align="center">
+ <h1>LogonJR Photo Gallery</h1>
   <h3>List of Photos</h3>
      <a href="photo1.php">Photo #1</a><br>
      <a href="photo2.php">Photo #2</a><br>
-     <a href="photo3.php">Photo #3</a><br></center>
+     <a href="photo3.php">Photo #3</a><br>
  <form action="index.php>
-  <h6>This is not you?</h6>
-  <button type="submit">Log Out</button>
+  <label This is not you?
+  <button type="submit" name="log">Log Out</button>
  </form>
 </body>
 </html>
