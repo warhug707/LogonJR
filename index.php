5a3c520d323911edbbf2435e49040e32ae6fde84
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-14 09:30:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 <head><title>LogonJR</title>
 </head>
 <body>
-<main>
+<main align="center">
  <form>
    <label for="user"><b>Username:</b></label>
-   <input type="text" placeholder="Enter Username name="user" required><br/>
+   <input type="text" placeholder="Enter Username" name="user" required><br/>
    
    <label for="pass"><b>Passwor:</b></label>
    <input type="password" placeholder="Enter Password" name="pass" required><br/>
    <button type="submit">Login</button><br/>
    
-   <a href="" align="center">Reset Password</a>
-   <a href="" align="center">Sign up</a>
+   <a href="">Reset Password</a><br/>
+   <a href="">Sign up</a>
 </body>
 </html>
