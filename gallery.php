700b19c34cd95f778ca23b68629a387926b4a579
MrEthical <51168679+warhug707@users.noreply.github.com>
2019-08-14 21:00:13 +0800

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head><title>HOME PAGE</title></head>
 <body>
+ <form action="index.php>
  <center><h1>LogonJR Photo Gallery</h1>
   <h3>List of Photos</h3>
      <a href="photo1.php">Photo #1</a><br>
      <a href="photo2.php">Photo #2</a><br>
      <a href="photo3.php">Photo #3</a><br>
- <form action="index.php>
   <button type="submit">Log Out</button>
  </form>
 </body>
